ffaaf12efea40cb2223b202ad95d92928e5cbb69
Ksi <labani.said@outlook.com>
2024-03-10 11:51:49 +0100

    Add User and UserTests

---
+++ src/Core/Domain/User.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="User.cs" company="KsiProgramming">
+// Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ContractAnalyzer
+{
+    public class User
+    {
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
---
+++ tests/Core.Tests/Domain/UserTests.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UserTests.cs" company="KsiProgramming">
+//     Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ContractAnalyzer.Tests
+{
+    using FluentAssertions;
+
+    public class UserTests
+    {
+        [Fact]
+        public void ShouldBeInitializedWithCorrectNames()
+        {
+            var user = new User(
+                firstName: "First Name",
+                lastName: "Last Name");
+
+            user.FirstName.Should().Be("First Name");
+            user.LastName.Should().Be("Last Name");
+        }
+    }
+}
