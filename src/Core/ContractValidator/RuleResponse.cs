33cdcfd12e923ac4d62960d16c2dfcc01c679654
Ksi <labani.said@outlook.com>
2024-03-10 15:24:09 +0100

    Add Rule reponse with tests

---
+++ src/Core/ContractValidator/RuleResponse.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RuleResponse.cs" company="KsiProgramming">
+// Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ContractAnalyzer.ContractValidator
+{
+    public class RuleResponse
+    {
+        public RuleResponse(string name, bool isInViolation)
+        {
+            this.Name = name;
+            this.IsInViolation = isInViolation;
+        }
+
+        public string Name { get; }
+
+        public bool IsInViolation { get; }
+    }
+}
---
+++ tests/Core.Tests/ContractValidator/RulesResponseTests.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RulesResponseTests.cs" company="KsiProgramming">
+// Copyright (c) KsiProgramming. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ContractAnalyzer.ContractValidator.Tests
+{
+    using FluentAssertions;
+
+    public class RulesResponseTests
+    {
+        [Fact]
+        public void ShouldBeInitializedWithCorrectProperties()
+        {
+            var rules = new RuleResponse(name: "Rule Number 1", isInViolation: true);
+
+            rules.Name.Should().Be("Rule Number 1");
+            rules.IsInViolation.Should().BeTrue();
+        }
+    }
+}
